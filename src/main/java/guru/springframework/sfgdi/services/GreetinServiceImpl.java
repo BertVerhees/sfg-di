19b6fbd345395a9b462d92178ed58da366acde45
BertVerheesGeonovum <b.verhees@geonovum.nl>
2020-03-28 17:04:22 +0100

    spring context

--- src/main/java/guru/springframework/sfgdi/services/GreetinServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetinServiceImpl.java
@@ -1,5 +1,8 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetinServiceImpl implements GreetingService {
 
     @Override
